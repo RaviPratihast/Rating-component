d210933b128991850a1a94818cdf68c39b215919
RAVI SHANKAR PRATIHAST <75324945+RaviPratihast@users.noreply.github.com>
2022-09-23 21:50:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-const ratings=document.querySelectorAll('.box');
-const ratingBox=document.querySelector('.rating-boxes-container');
+const ratings = document.querySelectorAll('.box');
+const ratingBox = document.querySelector('.rating-boxes-container');
+
+const submit=document.querySelector('.submit');
 
 
 // ratings.forEach((e)=>console.log(e.innerText));
+// here the rated value will be stored.
+let rating;
 
-ratingBox.addEventListener('click',function(e){
+ratingBox.addEventListener('click', function (e) {
 
-    if(e.target.classList.contains('box')){
+    if (e.target.classList.contains('box')) {
 
-        console.log(e.target.textContent)
+        // console.log(e.target.textContent)
+        if (rating === undefined) {
+            rating = Number(e.target.textContent);
+
+        }
+        console.log(typeof rating, rating);
+        // console.log(ratings.length);
+
     }
 });
+
+
+// submition part
+
+submit.addEventListener('click',function(){
+
+
+    console.log(`you have selected ${rating} out of ${ratings.length}`)
+})
