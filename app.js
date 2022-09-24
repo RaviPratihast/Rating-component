2ed358a612cb2dd1a0c52ce8362ff4c4a461ffee
RAVI SHANKAR PRATIHAST <75324945+RaviPratihast@users.noreply.github.com>
2022-09-24 10:20:22 +0530

    ADD

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const ratingBox = document.querySelector('.rating-boxes-container');
 
 const submit=document.querySelector('.submit');
 
+const div=document.querySelector('.main-container');
+
 
 // ratings.forEach((e)=>console.log(e.innerText));
 // here the rated value will be stored.
@@ -29,5 +31,24 @@ ratingBox.addEventListener('click', function (e) {
 submit.addEventListener('click',function(){
 
 
-    console.log(`you have selected ${rating} out of ${ratings.length}`)
+    // console.log(`you have selected ${rating} out of ${ratings.length}`)
+
+    div.innerHTML=`
+    <div class="after-rating-svg-container"><img src="/images/illustration-thank-you.svg" alt=""></div>
+    <div class="you-selected">
+        <h3>You Selected ${rating} out of ${ratings.length}</h3>
+    </div>
+    <div class="text-container-thankyou">
+        <div class="heading-thankyou">
+            <h1>Thank you!</h1>
+        </div>
+
+        <div class="thankyou-para"><p>
+            We appreciate you taking time to give a rating, if you ever need more support, don't hesitate to get in touch
+        </p></div>
+        
+    </div>
+    `
+
+
 })
